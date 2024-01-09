037f2c2ac6e74ffc28a327a114e273a94ac8267d
Samiya Kazi <samiya.kazi09@gmail.com>
2024-01-09 14:23:47 +0600

    fix: rule routes

--- server/src/routers/ruleRouter.ts
+++ server/src/routers/ruleRouter.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { getRulesForRestaurant, setRulesRestaurant } from '../controllers/rules.contoller';
 
 const Rulerouter = express.Router();
-Rulerouter.post('/rules/:restaurantId', setRulesRestaurant);
-Rulerouter.get('/rules/:restaurantId', getRulesForRestaurant);
+Rulerouter.post('/add', setRulesRestaurant);
+Rulerouter.get('/get', getRulesForRestaurant);
 export default  Rulerouter;
 
