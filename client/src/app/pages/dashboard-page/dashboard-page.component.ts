78aa5fd727e796a8f5f59f5cb33faaccc9e73cdd
Samiya Kazi <samiya.kazi09@gmail.com>
2024-01-13 15:57:36 +0600

    feat: added loaders for display and dashboard page

--- client/src/app/pages/dashboard-page/dashboard-page.component.ts
+++ client/src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -9,7 +9,7 @@ import { IUser } from '../../interfaces/user.interface';
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
-  styleUrl: './dashboard-page.component.css'
+  styleUrl: './dashboard-page.component.css',
 })
 export class DashboardPageComponent implements OnInit {
   totalOrders: number = 0;
@@ -19,38 +19,49 @@ export class DashboardPageComponent implements OnInit {
   servedOnTime: number = 0;
   servedOutOfTime: number = 0;
 
+  loading: boolean = false;
+
   chefs: IUser[] = [];
+  chefStats: { chef: IUser, totalServed: number, servedOnTime: number, servedOutOfTime: number }[] = [];
 
   constructor(
-    private ordersService: OrdersService, 
+    private ordersService: OrdersService,
     private loadingService: LoadingService,
     private chefService: ChefService
-    ) {}
+  ) {}
 
   ngOnInit(): void {
     this.setOrders(this.ordersService.orders);
-    this.loadingService.orderLoadingEvent.subscribe(() => 
-      this.setOrders(this.ordersService.orders)
-    );
+    this.loading = this.loadingService.orderLoading;
+    this.loadingService.orderLoadingEvent.subscribe((value) => {
+      this.loading = value;
+      this.setOrders(this.ordersService.orders);
+    });
 
     this.ordersService.newOrder.subscribe(() => this.pendingOrders++);
-    this.chefs = this.chefService.chefs;
   }
 
-
-  setOrders (orders: OrderItemInterface[]) {
+  setOrders(orders: OrderItemInterface[]) {
     this.totalOrders = orders.length;
-    this.pendingOrders = orders.filter((order) => order.status === 'pending').length;
-    this.preparingOrders = orders.filter((order) => order.status === 'preparing').length;
-    this.servedOrders = orders.filter((order) => order.status === 'complete').length;
-    const targetDeliveryTime = new Date(); 
+    this.pendingOrders = orders.filter(
+      (order) => order.status === 'pending'
+    ).length;
+    this.preparingOrders = orders.filter(
+      (order) => order.status === 'preparing'
+    ).length;
+    this.servedOrders = orders.filter(
+      (order) => order.status === 'complete'
+    ).length;
+    const targetDeliveryTime = new Date();
 
     this.servedOnTime = orders.filter((order) => {
-      const deliveryTime = new Date(order.deliveryTimestamp); 
-      return deliveryTime <= targetDeliveryTime;
+      if (!order.preparingTimestamp || !order.readyTimestamp) return false;
+      if (order.status !== "complete" && order.status !== "ready") return false;
+      const totalPrepTime = order.items.reduce((total, item) => item.itemPreparationTime + total, 0);
+      const prepTime = ((new Date(order.readyTimestamp)).getTime() - (new Date(order.preparingTimestamp)).getTime()) / 60000;
+      return prepTime < totalPrepTime;
     }).length;
 
     this.servedOutOfTime = this.servedOrders - this.servedOnTime;
   }
 }
-  
