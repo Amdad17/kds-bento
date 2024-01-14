a1060d0031321bb32f69bf52251317a0c3fb5624
Samiya Kazi <samiya.kazi09@gmail.com>
2024-01-14 17:13:18 +0600

    fix: socket room id

--- server/src/controllers/chef.controller.ts
+++ server/src/controllers/chef.controller.ts
@@ -7,7 +7,7 @@ export async function chefCheckIn (req: AuthRequest, res: Response) {
     const { user } = req;
     if (!user) return res.status(401).send({ message: 'Unauthorized.' });
     const io = res.locals.io;
-    io.to(user.employeeInformation.restaurantId).emit('chef-check-in', { chef: user });
+    io.to(user.employeeInformation.restaurantId.toString()).emit('chef-check-in', { chef: user });
     res.send({ status: "success" });
   } catch (error) {
     console.log(error);
@@ -21,7 +21,7 @@ export async function chefCheckOut (req: AuthRequest, res: Response) {
     const { user } = req;
     if (!user) return res.status(401).send({ message: 'Unauthorized.' });
     const io = res.locals.io;
-    io.to(user.employeeInformation.restaurantId).emit('chef-check-out', { chef: user });
+    io.to(user.employeeInformation.restaurantId.toString()).emit('chef-check-out', { chef: user });
     res.send({ status: "success" });
   } catch (error) {
     console.log(error);
