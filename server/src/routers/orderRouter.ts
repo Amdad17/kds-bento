5875d4a30b13ccdb8b89f9160a2b841172f1e8e9
Samiya Kazi <samiya.kazi09@gmail.com>
2024-01-17 15:17:41 +0600

    fix: added missing chef route

--- server/src/routers/orderRouter.ts
+++ server/src/routers/orderRouter.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import { changeOrderStatus, findOrdersByRestaurantId, incomingOrder } from '../controllers/order.contoller';
+import { addChefToOrder, changeOrderStatus, findOrdersByRestaurantId, incomingOrder } from '../controllers/order.contoller';
 
 const orderrouter = express.Router();
 
 orderrouter.post('/incoming', incomingOrder);
 orderrouter.put('/status', changeOrderStatus);
-// orderrouter.put('/chef/:orderId', addChefToOrder);
+orderrouter.put('/chef/:orderId', addChefToOrder);
 orderrouter.get('/restaurant', findOrdersByRestaurantId);
 
 export default orderrouter;
