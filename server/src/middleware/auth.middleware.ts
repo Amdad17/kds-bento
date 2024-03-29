847b4a1b188b1bc9b80c9f8f138ec18d2ffd70b0
Amdad Hossain <55600095+Amdad17@users.noreply.github.com>
2024-03-29 23:13:10 +0600

    remove useless console log

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -3,7 +3,7 @@ import { getUserFromToken } from "../services/skeleton.service";
 import { AuthRequest } from "../interfaces/authRequest.interface";
 
 export async function authMiddleware (req: AuthRequest, res: Response, next: NextFunction) {
-  // console.log('auth headers=========');
+  
   try {
     const authHeaders = req.headers["authorization"];
     if (!authHeaders) return res.status(401).send({ message: "Unauthorized" });
